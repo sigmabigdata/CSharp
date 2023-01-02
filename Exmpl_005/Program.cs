a8ab3a06856227c7162a7e3116d434de28e23436
sigmabigdata <sigmabigdata@gmail.com>
2023-01-02 18:33:18 +0300

    Final

---
+++ Exmpl_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользоватеоя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет " );
+    Console.WriteLine(username);
+}
